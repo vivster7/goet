e71f27d6c781f061622c453bb1cbc136829466c7
Vivek Dasari <vivek.dasari@benchling.com>
2021-06-16 22:46:57 -0700

    converter recursive

--- goet/lib/converter/converter.py
+++ goet/lib/converter/converter.py
@@ -1,7 +1,6 @@
-
 from collections import defaultdict
 import json
-from collections.abc import Set
+from collections.abc import Set, Mapping
 from base64 import b85encode
 from datetime import datetime
 from typing import Any, Union
@@ -9,25 +8,47 @@ from typing import Any, Union
 from cattr.converters import Converter, GenConverter
 
 
+def is_jsonable_primitive(typ: type) -> bool:
+    return typ in (str, int, float, bool, type(None))
+
+
+def is_jsonable_container(typ: type) -> bool:
+    return typ in (list, tuple, dict, set, frozenset)
+
+
 def is_jsonable(typ: type) -> bool:
-    return typ in (str, int, float, bool, list, tuple, dict, type(None))
+    return is_jsonable_primitive(typ) or is_jsonable_container(typ)
+
 
 def is_not_jsonable(obj: Any) -> bool:
     return not is_jsonable(obj)
 
+
 def unstructure_not_jsonable(obj: Any) -> Union[list, dict]:
+    # Handles recursive cases
+    if is_jsonable_primitive(type(obj)):
+        return obj
+
     unknown_name = "<unknown>"
     if callable(obj):
-        name = getattr(obj, '__name__', unknown_name)
+        name = getattr(obj, "__name__", unknown_name)
         return {name: f"<function {name}>"}
-    elif hasattr(obj, '__slots__'):
-        return {slot: getattr(obj, slot) for slot in obj.__slots__ if not slot.startswith('__')}
-    elif hasattr(obj, '__dict__'):
-        return {k: v for k, v in obj.__dict__.items() if not k.startswith('__')}
+    elif hasattr(obj, "__slots__"):
+        return {
+            slot: unstructure_not_jsonable(getattr(obj, slot))
+            for slot in obj.__slots__
+            if not slot.startswith("__")
+        }
+    elif hasattr(obj, "__dict__"):
+        return {
+            k: unstructure_not_jsonable(v)
+            for k, v in obj.__dict__.items()
+            if not k.startswith("__")
+        }
     elif isinstance(obj, defaultdict):
-        return {k: v for k, v in obj.items()}
-    elif hasattr(obj, '__iter__'):
-        return list(iter(obj))
+        return {k: unstructure_not_jsonable(v) for k, v in obj.items()}
+    elif hasattr(obj, "__iter__"):
+        return [unstructure_not_jsonable(x) for x in iter(obj)]
     else:
         # return {repr(obj): unknown_name}
         return {}
@@ -45,13 +66,13 @@ def configure_converter(converter: Converter):
 
     def unstructure_bytes(b: bytes) -> str:
         return (b85encode(b) if b else b"").decode("utf8")
-    converter.register_unstructure_hook(bytes, unstructure_bytes)
 
+    converter.register_unstructure_hook(bytes, unstructure_bytes)
 
     def unstructure_datetime(dt: datetime) -> str:
         return dt.isoformat()
-    converter.register_unstructure_hook(datetime, unstructure_datetime)
 
+    converter.register_unstructure_hook(datetime, unstructure_datetime)
 
     converter.register_unstructure_hook_func(is_not_jsonable, unstructure_not_jsonable)
 
@@ -60,6 +81,7 @@ def make_converter(*args, **kwargs) -> GenConverter:
     kwargs["unstruct_collection_overrides"] = {
         **kwargs.get("unstruct_collection_overrides", {}),
         Set: list,
+        Mapping: dict,
         # Counter: dict,
     }
     converter = GenConverter(*args, **kwargs)
--- goet/lib/converter/converter_test.py
+++ goet/lib/converter/converter_test.py
@@ -17,6 +17,7 @@ def test(x: Any, y: Any) -> None:
     except AssertionError as e:
         print(f"{val} != {y}")
 
+
 ## Primitives
 test(1, "1")
 test(0, "0")
@@ -24,12 +25,13 @@ test(-1, "-1")
 test(3.13, "3.13")
 test(1e100, "1e+100")
 test(1e-100, "1e-100")
-test(float('infinity'), "Infinity")
-test(float('-infinity'), "-Infinity")
-test(float('NaN'), "NaN")
+test(float("infinity"), "Infinity")
+test(float("-infinity"), "-Infinity")
+test(float("NaN"), "NaN")
 test(True, "true")
 test(False, "false")
-test("a", "\"a\"")
+test(None, "null")
+test("a", '"a"')
 
 # Containers
 test((), "[]")
@@ -41,53 +43,87 @@ test(Counter(), "{}")
 test(defaultdict(), "{}")
 
 # Bytes
-test(b'123', "\"F)}j\"")
+test(b"123", '"F)}j"')
 
 # Datetimes
-test(datetime(1,2,3,4,5,6,7), "\"0001-02-03T04:05:06.000007\"")
+test(datetime(1, 2, 3, 4, 5, 6, 7), '"0001-02-03T04:05:06.000007"')
 
 # Custom classes
 test(object(), "{}")
 
+
 class A:
     pass
+
+
 test(A(), "{}")
+
 
 class B:
     def __init__(self, x):
         self.x = x
+
+
 test(B(x=1), '{"x": 1}')
+
 
 @attr.define
 class C:
     x: int
+
+
 test(C(x=1), '{"x": 1}')
+
 
 @attr.frozen
 class D:
     x: int
+
+
 test(D(x=1), '{"x": 1}')
+
 
 @dataclass
 class E:
     x: int
+
+
 test(E(x=1), '{"x": 1}')
+
+
+class F:
+    def __init__(self, f):
+        self.f = f
+
+
+test(F(f=F(f=None)), json.dumps({"f": {"f": None}}))
 
 # Functions
 def f():
     return
+
+
 test(f, '{"f": "<function f>"}')
+
 
 def g():
     return f
-test(g, '{"g": "<function g>"}')    
+
+
+test(g, '{"g": "<function g>"}')
+
 
 def h(x):
     return x
+
+
 test(h, '{"h": "<function h>"}')
+
 
 def i():
     return i
+
+
 test(i, '{"i": "<function i>"}')
 
 test(lambda: 1, '{"<lambda>": "<function <lambda>>"}')
