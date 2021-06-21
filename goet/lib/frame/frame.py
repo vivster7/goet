c537e6629c9f3a1f6e2b7d223cc5ac9a579415fc
Vivek Dasari <vivek.dasari@benchling.com>
2021-06-20 22:31:20 -0700

    clean up frame

--- goet/lib/frame/frame.py
+++ goet/lib/frame/frame.py
@@ -1,22 +1,12 @@
 from typing import Any, Dict, Optional, Tuple
-from collections.abc import Callable
 
 import attr
-import cattr
-from cattr.converters import Converter, GenConverter
 from goet.lib.converter.converter import make_converter
 import json
-from hypothesis import strategies as st
-
-
-st.register_type_strategy(Any, st.from_type(type).flatmap(st.from_type))
-# st.register_type_strategy(object, lambda obj: repr(obj))
-# st.register_type_strategy(bytes, lambda x: repr(x))
-# st.register_type_strategy(Callable, lambda callable: repr(callable))
 
 
 @attr.resolve_types
-@attr.define
+@attr.frozen
 class Code:
     """Code objects represent byte-compiled executable Python code, or bytecode.
 
@@ -27,21 +17,21 @@ class Code:
 
     Special read-only attributes:
         co_name gives the function name
-        co_argcount is the total number of positional arguments (including positional-only arguments and arguments with default values)
-        co_posonlyargcount is the number of positional-only arguments (including arguments with default values)
-        co_kwonlyargcount is the number of keyword-only arguments (including arguments with default values)
-        co_nlocals is the number of local variables used by the function (including arguments)
+        # co_argcount is the total number of positional arguments (including positional-only arguments and arguments with default values)
+        # co_posonlyargcount is the number of positional-only arguments (including arguments with default values)
+        # co_kwonlyargcount is the number of keyword-only arguments (including arguments with default values)
+        # co_nlocals is the number of local variables used by the function (including arguments)
         co_varnames is a tuple containing the names of the local variables (starting with the argument names)
         co_cellvars is a tuple containing the names of local variables that are referenced by nested functions
         co_freevars is a tuple containing the names of free variables
-        co_code is a string representing the sequence of bytecode instructions
-        co_consts is a tuple containing the literals used by the bytecode
-        co_names is a tuple containing the names used by the bytecode
+        # co_code is a string representing the sequence of bytecode instructions
+        # co_consts is a tuple containing the literals used by the bytecode
+        # co_names is a tuple containing the names used by the bytecode
         co_filename is the filename from which the code was compiled
         co_firstlineno is the first line number of the function
-        co_lnotab is a string encoding the mapping from bytecode offsets to line numbers (for details see the source code of the interpreter)
+        # co_lnotab is a string encoding the mapping from bytecode offsets to line numbers (for details see the source code of the interpreter)
         co_stacksize is the required stack size
-        co_flags is an integer encoding a number of flags for the interpreter.
+        # co_flags is an integer encoding a number of flags for the interpreter.
 
     The following flag bits are defined for co_flags: bit 0x04 is set if the function uses the *arguments syntax to accept an arbitrary number of positional arguments
         bit 0x08 is set if the function uses the **keywords syntax to accept arbitrary keyword arguments
@@ -56,55 +46,55 @@ class Code:
     """
 
     co_name: str
-    co_argcount: int
-    co_posonlyargcount: int
-    co_kwonlyargcount: int
-    co_nlocals: int
-    # co_varnames: Tuple[str]
-    # co_cellvars: Tuple[str]
-    # co_freevars: Tuple[str]
-    co_code: bytes
+    # co_argcount: int
+    # co_posonlyargcount: int
+    # co_kwonlyargcount: int
+    # co_nlocals: int
+    co_varnames: Tuple[str]
+    co_cellvars: Tuple[str]
+    co_freevars: Tuple[str]
+    # co_code: bytes
     # co_consts: Tuple[str, int, float, bool]
     # co_names: Tuple[str]
     co_filename: str
     co_firstlineno: int
-    co_lnotab: bytes
+    # co_lnotab: bytes
     co_stacksize: int
-    co_flags: int
+    # co_flags: int
 
     @classmethod
     def from_syscode(cls, syscode):
         return cls(
             co_name=syscode.co_name,
-            co_argcount=syscode.co_argcount,
-            co_posonlyargcount=syscode.co_posonlyargcount,
-            co_kwonlyargcount=syscode.co_kwonlyargcount,
-            co_nlocals=syscode.co_nlocals,
-            # co_varnames=syscode.co_varnames,
-            # co_cellvars=syscode.co_cellvars,
-            # co_freevars=syscode.co_freevars,
-            co_code=syscode.co_code,
+            # co_argcount=syscode.co_argcount,
+            # co_posonlyargcount=syscode.co_posonlyargcount,
+            # co_kwonlyargcount=syscode.co_kwonlyargcount,
+            # co_nlocals=syscode.co_nlocals,
+            co_varnames=syscode.co_varnames,
+            co_cellvars=syscode.co_cellvars,
+            co_freevars=syscode.co_freevars,
+            # co_code=syscode.co_code,
             # co_consts=syscode.co_consts,
             # co_names=syscode.co_names,
             co_filename=syscode.co_filename,
             co_firstlineno=syscode.co_firstlineno,
-            co_lnotab=syscode.co_lnotab,
+            # co_lnotab=syscode.co_lnotab,
             co_stacksize=syscode.co_stacksize,
-            co_flags=syscode.co_flags,
+            # co_flags=syscode.co_flags,
         )
 
 
-@attr.define
+@attr.frozen
 class Frame:
     """Frame objects represent execution frames. They may occur in traceback objects (see below), and are also passed to registered trace functions.
 
     Special read-only attributes:
         f_back is to the previous stack frame (towards the caller), or None if this is the bottom stack frame
-        f_code is the code object being executed in this frame
+        # f_code is the code object being executed in this frame
         f_locals is the dictionary used to look up local variables
-        f_globals is used for global variables
-        f_builtins is used for built-in (intrinsic) names
-        f_lasti gives the precise instruction (this is an index into the bytecode string of the code object).
+        # f_globals is used for global variables
+        # f_builtins is used for built-in (intrinsic) names
+        # f_lasti gives the precise instruction (this is an index into the bytecode string of the code object).
 
     Special writable attributes:
         f_trace, if not None, is a function called for various events during code execution (this is used by the debugger).
@@ -117,89 +107,46 @@ class Frame:
     A debugger can implement a Jump command (aka Set Next Statement) by writing to f_lineno.
     """
 
-    f_back: Optional["Frame"]
-    f_code: Code
+    f_id: int
+    f_back_id: int
+    f_filename: str
+
+    # Fields from Python
+    # f_back: Optional["Frame"]
+    # f_code: Code
     f_locals: Dict[str, Any]
     # f_globals: Dict[str, Any]
     # f_builtins: Dict[str, Any]
-    f_lasti: int
-    f_trace: Optional[Callable]
-    f_trace_lines: bool
-    f_trace_opcodes: bool
+    # f_lasti: int
+    # f_trace: Optional[Callable]
+    # f_trace_lines: bool
+    # f_trace_opcodes: bool
     f_lineno: int
 
     @classmethod
-    def from_sysframe(cls, sysframe):
+    def from_sysframe(cls, sysframe, f_id, f_back_id):
         return cls(
-            f_back=Frame.from_sysframe(sysframe.f_back) if sysframe.f_back else None,
-            f_code=Code.from_syscode(sysframe.f_code),
+            f_id=f_id,
+            f_back_id=f_back_id,
+            f_filename=sysframe.f_code.co_filename,
+            # f_back=None,
+            # f_back=Frame.from_sysframe(sysframe.f_back) if sysframe.f_back else None,
+            # f_code=Code.from_syscode(sysframe.f_code),
             f_locals=sysframe.f_locals,
             # f_globals=sysframe.f_globals,
             # f_builtins=sysframe.f_builtins,
-            f_lasti=sysframe.f_lasti,
-            f_trace=sysframe.f_trace,
-            f_trace_lines=sysframe.f_trace_lines,
-            f_trace_opcodes=sysframe.f_trace_opcodes,
+            # f_lasti=sysframe.f_lasti,
+            # f_trace=sysframe.f_trace,
+            # f_trace_lines=sysframe.f_trace_lines,
+            # f_trace_opcodes=sysframe.f_trace_opcodes,
             f_lineno=sysframe.f_lineno,
         )
-    
+
     def to_json(self):
-        # import pickle
-        # converter = GenConverter()
-        # def unstructure_object(obj: Any) -> Any:
-        #     try:
-        #         return pickle.dumps(obj)
-        #     except TypeError:
-        #         print(f'{obj=} is not pickleable')
-        #         return str(obj)
-
-        # converter.register_unstructure_hook(Frame, converter.unstructure_attrs_asdict)
-        # converter.register_unstructure_hook(Code, converter.unstructure_attrs_asdict)
-        # converter.register_unstructure_hook(object, unstructure_object)
-
-        # unstructured = converter.unstructure(self)
-        # print(f"{unstructured=}")
-        # return json.dumps(unstructured)
-
         converter = make_converter()
         unstructured = converter.unstructure(self)
         return json.dumps(unstructured)
-        # first_pass = converter.unstructure(self)
-
-        # def is_jsonable(obj):
-        #     jsonable_types = (str, int, float, bool, list, tuple, dict, type(None))
-        #     return isinstance(obj, jsonable_types)
-
-        # def is_not_jsonable(obj):
-        #     return not is_jsonable(obj)
-
-        # def unstructure_not_jsonable(obj):
-        #     for attr in dir(obj):
-        #         if attr.startswith('__'):
-        #             continue
-        #         elif callable(obj.attr):
-        #             return json.dumps({attr: repr(obj.attr)})
-        #         elif is_jsonable(obj.attr):
-        #             return json.dumps({attr: obj.attr})
-        #         else:
-        #             return unstructure_not_jsonable(obj.attr)
-
-                
-
-        # c2 = Converter()
-        # c2.register_unstructure_hook_func(is_not_jsonable, unstructure_not_jsonable)
-        # second_pass = c2.unstructure(first_pass)
-
-        # return json.dumps(second_pass)
 
 
 # Must be called after Frame is defined to resolve the `f_back: Frame` field.
 attr.resolve_types(Frame, globals(), locals())
-
-# from cattr.preconf.json import make_converter
-# converter = make_converter()
-# child = st.from_type(Frame).example()
-# child.f_back = st.from_type(Frame).example()
-# # child.f_code.co_code = b'123'
-# print(json.dumps(converter.unstructure(child)))
-# print(child == converter.structure(converter.unstructure(child), Frame))
